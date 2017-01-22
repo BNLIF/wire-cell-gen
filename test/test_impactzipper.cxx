110699ac7c02fc7b059ffc66865b756436b5ae44
Brett Viren <brett.viren@gmail.com>
2017-01-22 10:11:18 -0500

    Fix display binning.

--- test/test_impactzipper.cxx
+++ test/test_impactzipper.cxx
@@ -85,21 +85,39 @@ int main(int argc, char *argv[])
     const double dedx = 7.0e4/(1*units::cm); // in electrons
     const double charge_per_depo = -(dedx)*stepsize;
 
-    // mostly "prolonged" track in X direction
-    tracks.add_track(t0 + 1*units::ms,
-                     Ray(Point(1*units::m, 0*units::m, 0*units::m),
-                         Point(2*units::m, 0*units::m, 3*units::cm)), // over 10 wires
+    // // mostly "prolonged" track in X direction
+    // tracks.add_track(t0 + 1*units::ms,
+    //                  Ray(Point(1*units::m, 0*units::m, 0*units::m),
+    //                      Point(2*units::m, 0*units::m, 3*units::cm)), // over 10 wires
+    //                  charge_per_depo);
+    // // mostly "isochronous" track in Z direction
+    // tracks.add_track(t0 + 2*units::ms,
+    //                  Ray(Point(1*units::m, 0*units::m, -1*units::m),
+    //                      Point(1*units::m + 100*units::us*drift_speed, 0*units::m, +1*units::m)),
+    //                  charge_per_depo);
+    // // "driftlike" track diagonal in space and drift time
+    // tracks.add_track(t0 + 3*units::ms,
+    //                  Ray(Point(1*units::m, 0*units::m, -1*units::m),
+    //                      Point(2*units::m, 0*units::m, +1*units::m)),
+    //                  charge_per_depo);
+
+    // make a +
+    // tracks.add_track(t0 + 0.5*units::ms,
+    //                  Ray(Point(1.5*units::m, 0*units::m, -1*units::m),
+    //                      Point(1.5*units::m, 0*units::m, +1*units::m)),
+    //                  charge_per_depo);
+    // tracks.add_track(t0 + 0.5*units::ms,
+    //                  Ray(Point(1.5*units::m, -1*units::m, 0*units::m),
+    //                      Point(1.5*units::m, +1*units::m, 0*units::m)),
+    //                  charge_per_depo);
+
+
+    // make a .
+    tracks.add_track(t0 + 0.5*units::ms,
+                     Ray(Point(1.5*units::m, 0*units::m, -0.3*units::mm),
+                         Point(1.5*units::m, 0*units::m, +0.3*units::mm)),
                      charge_per_depo);
-    // mostly "isochronous" track in Z direction
-    tracks.add_track(t0 + 2*units::ms,
-                     Ray(Point(1*units::m, 0*units::m, -1*units::m),
-                         Point(1*units::m + 100*units::us*drift_speed, 0*units::m, +1*units::m)),
-                     charge_per_depo);
-    // "driftlike" track diagonal in space and drift time
-    tracks.add_track(t0 + 3*units::ms,
-                     Ray(Point(1*units::m, 0*units::m, -1*units::m),
-                         Point(2*units::m, 0*units::m, +1*units::m)),
-                     charge_per_depo);
+
     em("made tracks");
 
     // Get depos
@@ -108,20 +126,23 @@ int main(int argc, char *argv[])
     em("made depos");
 
     TFile* rootfile = TFile::Open(Form("%s-uvw.root", argv[0]), "recreate");
+    TCanvas* canvas = new TCanvas("c","canvas",500,500);
+    gStyle->SetOptStat(0);
+    canvas->Print("test_impactzipper.pdf[","pdf");
 
     for (int plane_id = 0; plane_id < 3; ++plane_id) {
         Pimpos& pimpos = uvw_pimpos[plane_id];
 
         // add deposition to binned diffusion
-        Gen::BinnedDiffusion bdw(pimpos, tbins, ndiffision_sigma, fluctuate);
+        Gen::BinnedDiffusion bindiff(pimpos, tbins, ndiffision_sigma, fluctuate);
         em("made BinnedDiffusion");
         for (auto depo : depos) {
             auto drifted = std::make_shared<Gen::TransportedDepo>(depo, field_origin.x(), drift_speed);
 
-            const double sigma_time = 1.0*units::us; // fixme: should really be function of drift time
-            const double sigma_pitch = 1.0*units::mm; // fixme: ditto
+            const double sigma_time = 3.0*units::us; // fixme: should really be function of drift time
+            const double sigma_pitch = 3.0*units::mm; // fixme: ditto
 	
-            bool ok = bdw.add(drifted, sigma_time, sigma_pitch);
+            bool ok = bindiff.add(drifted, sigma_time, sigma_pitch);
             if (!ok) {
                 std::cerr << "failed to add: t=" << drifted->time()/units::us << ", pt=" << drifted->pos()/units::mm << std::endl;
             }
@@ -138,20 +159,23 @@ int main(int argc, char *argv[])
         PlaneImpactResponse pirw(fr, plane_id);
         em("made PlaneImpactResponse");
 
-        Gen::ImpactZipper zipper(pirw, bdw);
+        Gen::ImpactZipper zipper(pirw, bindiff);
         em("made ImpactZipper");
 
-        auto pitch_mm = bdw.pitch_range();
-        cerr << "Pitch sample range: ["<<pitch_mm.first/units::mm <<","<< pitch_mm.second/units::mm<<"]mm\n";
+        auto tmm = bindiff.time_range(ndiffision_sigma);
+        const int tbin0 = max(0, tbins.bin(tmm.first)-2);
+        const int tbinf = min(tbins.nbins()-1, tbins.bin(tmm.second) + 2);
+        const int ntbins = 1+tbinf-tbin0;
 
-        const auto rb = pimpos.region_binning();
-        const int minwire = rb.bin(pitch_mm.first);
-        const int maxwire = rb.bin(pitch_mm.second);
-        cerr << "Wires: [" << minwire << "," << maxwire << "]\n";
+        auto rbins = pimpos.region_binning();
+        auto pmm = bindiff.pitch_range(ndiffision_sigma);
+        const int wbin0 = max(0, rbins.bin(pmm.first) - 11);
+        const int wbinf = min(rbins.nbins()-1, rbins.bin(pmm.second) + 11);
+        const int nwbins = 1+wbinf-wbin0;
 
         std::map<int, Waveform::realseq_t> frame;
         double tottot=0.0;
-        for (int iwire=minwire; iwire <= maxwire; ++iwire) {
+        for (int iwire=wbin0; iwire <= wbinf; ++iwire) {
             auto wave = zipper.waveform(iwire);
             auto tot = Waveform::sum(wave);
             tottot += tot;
@@ -161,20 +185,6 @@ int main(int argc, char *argv[])
         em("zipped through wires");
         cerr << "Tottot = " << tottot << endl;
         Assert(tottot > 0.0);
-
-        auto tmm = bdw.time_range();
-        const int tbin0 = tbins.bin(tmm.first);
-        const int tbinf = tbins.bin(tmm.second);
-        const int ntbins = 1+tbinf-tbin0;
-
-        auto pmm = bdw.pitch_range();
-        const int wbin0 = rb.bin(pmm.first);
-        const int wbinf = rb.bin(pmm.second);
-        const int nwbins = 1+wbinf-wbin0;
-    
-        //TCanvas canvas("c","canvas",500,500);
-        gStyle->SetOptStat(0);
-    
 
         TH2F *hist = new TH2F(Form("h%d", plane_id),
                               Form("Wire vs Tick %c-plane", uvw[plane_id]),
@@ -188,7 +198,7 @@ int main(int argc, char *argv[])
 
         for (auto wire : frame) {
             const int iwire = wire.first;
-            Assert(rb.inbounds(iwire));
+            Assert(rbins.inbounds(iwire));
             const Waveform::realseq_t& wave = wire.second;
             auto tot = Waveform::sum(wave);
             //std::cerr << iwire << " tot=" << tot << std::endl;
@@ -198,6 +208,8 @@ int main(int argc, char *argv[])
             }
         }
         hist->Write();
+
+        hist->Draw("colz");
 
         std::vector<TLine*> lines;
         auto trqs = tracks.tracks();
@@ -213,16 +225,20 @@ int main(int argc, char *argv[])
             const int tick1 = tbins.bin(time + (ray.first.x()-fr.origin)/drift_speed);
             const int tick2 = tbins.bin(time + (ray.second.x()-fr.origin)/drift_speed);
             
-            const int wire1 = rb.bin(pimpos.distance(ray.first, plane_id));
-            const int wire2 = rb.bin(pimpos.distance(ray.second, plane_id));
+            const int wire1 = rbins.bin(pimpos.distance(ray.first, plane_id));
+            const int wire2 = rbins.bin(pimpos.distance(ray.second, plane_id));
             
             cerr << "digitrack: t=" << time << " ticks=["<<tick1<<","<<tick2<<"] wires=["<<wire1<<","<<wire2<<"]\n";
             
             TLine* line = new TLine(tick1, wire1, tick2, wire2);
             line->Write(Form("l%c%d", uvw[plane_id], iline));
+            line->Draw();
         }
+
+        canvas->Print("test_impactzipper.pdf","pdf");
     }
     rootfile->Close();
+    canvas->Print("test_impactzipper.pdf]","pdf");
     em("done");
 
     cerr << em.summary() << endl;
