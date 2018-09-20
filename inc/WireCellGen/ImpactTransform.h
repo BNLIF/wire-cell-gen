d7e840e948edbfc4c1d890b6520ce48d8f58ca66
Brett Viren <brett.viren@gmail.com>
2018-09-20 16:35:32 -0400

    Move Xin's work on a faster sim convolution into its own file, restore ImpactZipper to its former (slow) glory, add new components that break up Ductor functionality and some supporting DepoSet related components.

--- inc/WireCellGen/ImpactTransform.h
+++ inc/WireCellGen/ImpactTransform.h
@@ -1,27 +1,39 @@
-#ifndef WIRECELL_IMPACTZIPPER
-#define WIRECELL_IMPACTZIPPER
+#ifndef WIRECELL_IMPACTTRANSFORM
+#define WIRECELL_IMPACTTRANSFORM
 
 #include "WireCellIface/IPlaneImpactResponse.h"
 #include "WireCellGen/BinnedDiffusion.h"
+#include "WireCellUtil/Array.h"
+
 
 namespace WireCell {
     namespace Gen {
 
     
-        /** An ImpactZipper "zips" up through all the impact positions
-         * along a wire plane convolving the response functions and
-         * the local drifted charge distribution producing a waveform
-         * on each central wire.
+        /** An ImpactTransform transforms charge on impact positions
+         * into waveforms via 2D FFT.
          */
-        class ImpactZipper
+        class ImpactTransform
         {
             IPlaneImpactResponse::pointer m_pir;
             BinnedDiffusion& m_bd;
-
+	    
+	    int m_flag;
+	    int m_num_group;  // how many 2D convolution is needed
+	    int m_num_pad_wire; // how many wires are needed to pad on each side
+	    std::vector<std::map<int, IImpactResponse::pointer> > m_vec_map_resp;
+	    std::vector<std::vector<std::tuple<int,int,double> > > m_vec_vec_charge; // ch, time, charge
+	    std::vector<int> m_vec_impact;
+	    Array::array_xxf m_decon_data;
+	    int m_start_ch;
+	    int m_end_ch;
+	    int m_start_tick;
+	    int m_end_tick;
+	    
         public:
 
-            ImpactZipper(IPlaneImpactResponse::pointer pir, BinnedDiffusion& bd);
-            virtual ~ImpactZipper();
+            ImpactTransform(IPlaneImpactResponse::pointer pir, BinnedDiffusion& bd);
+            virtual ~ImpactTransform();
 
             /// Return the wire's waveform.  If the response functions
             /// are just field response (ie, instantaneous current)
@@ -35,8 +47,10 @@ namespace WireCell {
             // fixme: this should be a forward iterator so that it may cal bd.erase() safely to conserve memory
             Waveform::realseq_t waveform(int wire) const;
 
+
+	    
         };
 
     }  // Gen
 }  // WireCell
-#endif /* WIRECELL_IMPACTZIPPER */
+#endif /* WIRECELL_IMPACTTRANSFORM */
